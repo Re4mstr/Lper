d6a6356b702e7aa74b78233d5463207c95b1f850
Re4mstr <15211713+Re4mstr@users.noreply.github.com>
2019-06-02 00:57:52 +0200

    Update Lper.sh
    + function that runs code from remote main.sh file

--- Lper.sh
+++ Lper.sh
@@ -2,3 +2,6 @@
 
 # Description:
 # How to describe this?
+
+# Run the latest stable main script from master branch:
+curl -sSL https://raw.githubusercontent.com/Re4mstr/Lper/master/Scripts/main.sh | bash
